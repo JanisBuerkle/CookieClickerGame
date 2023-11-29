3883b2dc4f8e338a5486fa2913a95db0d602bc7c
Janis Bürkle <bks@teamtechnik.com>
2023-11-29 11:43:12 +0100

    29.11.2023 | 11:43

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace RPG;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private string content;
+
+    public string Content
+    {
+        get => content;
+        set
+        {
+            if (content != value)
+            {
+                content = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
