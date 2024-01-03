99e4911515e60c21ee3833eb2e4ebfae8143bf4f
Janis Bürkle <bks@teamtechnik.com>
2024-01-03 15:33:11 +0100

    03.01.2024 | 15:33

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,49 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RPG
 {
     public partial class MainWindow : Window
     {
-        private double Cookies { get; set; }
-        public double Multiplikator { get; set; }
         public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
-            nameof(ViewModel), typeof(MainWindowViewModel), typeof(MainWindow), new PropertyMetadata(default(MainWindowViewModel)));
+            nameof(ViewModel), typeof(MainWindowViewModel), typeof(MainWindow),
+            new PropertyMetadata(default(MainWindowViewModel)));
 
         public MainWindowViewModel ViewModel
         {
-            get => (MainWindowViewModel)GetValue(ViewModelProperty); 
-            set => SetValue(ViewModelProperty, value); 
+            get => (MainWindowViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
         }
-        
+
         public MainWindow()
         {
-            Multiplikator = 1.0;
             ViewModel = new MainWindowViewModel();
             InitializeComponent();
         }
 
-        private void CookieClicked(object sender, RoutedEventArgs e)
+        private void ExitButtonClicked(object sender, RoutedEventArgs e)
         {
-            ViewModel.Content = $"Cookies: {Cookies:F1}";
-            Cookies += Multiplikator;
+            Application.Current.Shutdown();
         }
 
-        private void Times01(object sender, RoutedEventArgs e)
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            Multiplikator += 0.1;
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }
--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,19 +1,107 @@
-﻿namespace RPG;
+﻿using System;
+using CommunityToolkit.Mvvm.Input;
+
+namespace RPG;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    private string content;
+    private string _content;
 
     public string Content
     {
-        get => content;
+        get => _content;
         set
         {
-            if (content != value)
+            if (_content != value)
             {
-                content = value;
+                _content = value;
                 OnPropertyChanged();
             }
         }
+    }
+
+    private bool _infoVisible;
+
+    public bool InfoVisible
+    {
+        get => _infoVisible;
+        set
+        {
+            if (value == _infoVisible) return;
+            _infoVisible = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private double Cookies { get; set; }
+    private double Multiplikator { get; set; } = 0.1;
+
+    public RelayCommand<object> Times { get; }
+
+    public RelayCommand Count { get; }
+
+    public MainWindowViewModel()
+    {
+        ContentRefresh();
+        Count = new RelayCommand(CountMethod);
+        Times = new RelayCommand<object>(TimesMethod);
+    }
+
+    private void TimesMethod(object zahl)
+    {
+        double price = Convert.ToDouble(zahl);
+        if (Cookies >= price)
+        {
+            Cookies -= price;
+            switch (zahl)
+            {
+                case "10":
+                    Multiplikator += 0.1;
+                    break;
+                case "50":
+                    Multiplikator += 0.5;
+                    break;
+                case "100":
+                    Multiplikator += 1;
+                    break;
+                case "200":
+                    Multiplikator += 2;
+                    break;
+                case "500":
+                    Multiplikator += 5;
+                    break;
+                case "1000":
+                    Multiplikator += 10;
+                    break;
+                case "2000":
+                    Multiplikator += 20;
+                    break;
+                case "5000":
+                    Multiplikator += 50;
+                    break;
+                case "10000":
+                    Multiplikator += 100;
+                    break;
+            }
+
+            InfoVisible = false;
+            ContentRefresh();
+        }
+        else
+        {
+            InfoVisible = true;
+        }
+    }
+
+    private void ContentRefresh()
+    {
+        Content = $"Cookies: {Cookies:F1}";
+    }
+
+    private void CountMethod()
+    {
+        ContentRefresh();
+        Cookies += Multiplikator;
+        InfoVisible = false;
     }
 }
