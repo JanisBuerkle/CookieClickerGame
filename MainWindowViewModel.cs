446c626a81d7c6450361a934e08a032a3643297a
Janis Bürkle <bks@teamtechnik.com>
2024-01-04 15:57:49 +0100

    Cookie Expanded

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Net;
+using System.Timers;
 using System.Xml.Serialization;
 using CommunityToolkit.Mvvm.Input;
 
@@ -7,6 +9,7 @@ namespace RPG
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private static Timer timer;
         private GameData GameData { get; set; }
 
         public MainWindowViewModel(GameData gameData, string cookieContent, string perClickContent)
@@ -20,6 +23,29 @@ namespace RPG
             ContentRefresh();
             Count = new RelayCommand(CountMethod);
             Times = new RelayCommand<object>(TimesMethod);
+            AFKCookieCommand = new RelayCommand(AfkCookieCommandMethod);
+        }
+
+        public bool Test { get; set; }
+        private void AfkCookieCommandMethod()
+        {
+            AfkCookie = GameData.Multiplikator;
+            if (!Test)
+            {
+                timer = new Timer(1000);
+                timer.Elapsed += OnTimedEvent;
+                timer.AutoReset = true;
+                timer.Enabled = true;
+                Test = true;
+            }
+            
+            if (GameData.Multiplikator != AfkCookie)
+            {
+                AfkBuyPrice *= 11;
+                Test = false;
+            }
+
+            
         }
 
         private void OnProcessExit(object sender, EventArgs e)
@@ -32,7 +58,16 @@ namespace RPG
             if (GameData.Multiplikator < 0.1)
             {
                 GameData.Multiplikator += 0.1;
+                AfkCookie = 0;
+                AfkBuyPrice = 1;
             }
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            GameData.Cookies += AfkCookie;
+            Console.Write("Test");
+            ContentRefresh();
         }
 
         private void TimesMethod(object zahl)
@@ -62,7 +97,7 @@ namespace RPG
                         GameData.Multiplikator += 10;
                         break;
                     case "2000":
-                        GameData.Multiplikator += 20; 
+                        GameData.Multiplikator += 20;
                         break;
                     case "5000":
                         GameData.Multiplikator += 50;
@@ -115,7 +150,7 @@ namespace RPG
                     case "1000000000":
                         GameData.Multiplikator += 10000000;
                         break;
-                    
+
                     case "2000000000":
                         GameData.Multiplikator += 200000000;
                         break;
@@ -177,8 +212,40 @@ namespace RPG
             }
         }
 
+
+        private int _afkBuyPrice;
+
+        public int AfkBuyPrice
+        {
+            get => _afkBuyPrice;
+            set
+            {
+                if (_afkBuyPrice != value)
+                {
+                    _afkBuyPrice = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         public RelayCommand<object> Times { get; }
         public RelayCommand Count { get; }
+        public RelayCommand AFKCookieCommand { get; }
+
+        private string _afkMoney;
+
+        public string AFKMoney
+        {
+            get => _afkMoney;
+            set
+            {
+                if (_afkMoney != value)
+                {
+                    _afkMoney = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         private string _cookieContent;
 
@@ -223,12 +290,15 @@ namespace RPG
             }
         }
 
+        private double AfkCookie { get; set; }
+
         private void ContentRefresh()
         {
             string formattedString = String.Format("{0:N1}", GameData.Cookies);
             formattedString = formattedString.Replace(",", ".");
             CookieContent = $"Cookies: {formattedString}";
             PerClickContent = $"Pro Click: {GameData.Multiplikator:F1}";
+            AFKMoney = $"AFK - {AfkCookie} Cookies/s | 10 Cookies";
         }
     }
 }
